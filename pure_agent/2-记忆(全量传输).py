4b91a6731554645f627d9bab8ee1ebc48d133fa1
lushuaiyan <517634546@qq.com>
2026-06-24 23:55:23 +0800

    补充了全量传输的记忆模块代码

--- pure_agent/2-记忆(全量传输).py
+++ pure_agent/2-记忆(全量传输).py
@@ -10,3 +10,19 @@ client= OpenAI(
     base_url="https://api.deepseek.com",
     api_key=os.environ.get("DEEPSEEK_API_KEY"),
 )
+
+messages = [
+    {"role": "system", "content": "你是一个很有用的助手, 小帅"}
+]
+
+while True:
+    query = input("请输入您的用户提示词或输入'/exit'退出\n")
+    if query=="/exit":
+        break
+    messages.append({"role": "user", "content": query})
+    resp = client.chat.completions.create(
+        model="deepseek-v4-pro",
+        messages=messages
+        )
+    print(resp.choices[0].message.content)
+    messages.append({"role": "assistant", "content": resp.choices[0].message.content})
